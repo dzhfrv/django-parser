53cac15997d53099035c8d974aefff89d7b53399
dzhfrv <solomonickey@gmail.com>
2020-03-03 17:38:55 +0200

    stats model

--- apps/stats/models.py
+++ apps/stats/models.py
@@ -1,3 +1,17 @@
+from django.contrib.postgres.fields import JSONField
 from django.db import models
+from apps.links.models import Link
 
-# Create your models here.
+
+class Stats(models.Model):
+    url = models.ForeignKey(
+        Link,
+        on_delete=models.CASCADE,
+        related_name='stats',
+    )
+    tags = JSONField('tags')
+
+    class Meta:
+        verbose_name = 'link'
+        verbose_name_plural = 'links'
+
