8dd0f41be155c605368ab5ebc6d891e51d922bf9
dzhfrv <solomonickey@gmail.com>
2020-03-03 14:13:00 +0200

    added link model & migration

--- apps/links/models.py
+++ apps/links/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Link(models.Model):
+    user = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='links',
+    )
+    link = models.CharField('link', max_length=200, blank=True)
+    status = models.IntegerField('status')
+    date_added = models.DateTimeField('date added', auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'link'
+        verbose_name_plural = 'links'
